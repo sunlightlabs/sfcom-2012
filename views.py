e146fa24419d5f8a2fc3988084c3b1e51b140057
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-11-04 16:03:10 -0400

    make real blog data even more accurate

--- views.py
+++ views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from sunlightfoundation.com.models import FeaturedPost
+from sunlightfoundation.com.models import FeaturedPost, FeaturedTopic
 
 def index(request):
     
     featured_posts = FeaturedPost.objects.filter(published=True).order_by("-date_published")[:2]
+    featured_topics = FeaturedTopic.objects.filter(published=True).order_by('-id')[:3]
     
     data = {
         'featured_posts': featured_posts,
+        'featured_topics': featured_topics,
     }
     
     return render_to_response('index.html', data, context_instance=RequestContext(request))
