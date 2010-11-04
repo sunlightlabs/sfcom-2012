d110d4f6d65b4e295426c539a515de0ca2be7c2f
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-11-04 18:59:24 -0400

    change privacy policy and terms to static templates instead of flatpages

--- urls.py
+++ urls.py
@@ -48,6 +48,8 @@ urlpatterns += patterns('django.views.generic.simple',
     url(r'^about/funding/$', 'direct_to_template', {"template": "about/funding.html"}),
     url(r'^about/grants/$', 'direct_to_template', {"template": "about/grants.html"}),
     url(r'^about/history/$', 'direct_to_template', {"template": "about/history.html"}),
+    url(r'^legal/privacy/$', 'direct_to_template', {"template": "legal/privacy.html"}),
+    url(r'^legal/terms/$', 'direct_to_template', {"template": "legal/terms.html"}),
 )
 
 
