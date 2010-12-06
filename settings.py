991ba460e16f607ea79b3b657179b05d8a8fdf6e
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-12-06 15:29:46 -0500

    update cache buster

--- settings.py
+++ settings.py
@@ -143,7 +143,7 @@ MEDIASYNC = {
     'AWS_PREFIX': 'site/4.0',
     'AWS_BUCKET_CNAME': True,
     'DOCTYPE': 'html5',
-    'CACHE_BUSTER': 201012061356,
+    'CACHE_BUSTER': 201012061529,
     'JOINED': {
         'styles/thefoundation.css': (
             'styles/style.css',
